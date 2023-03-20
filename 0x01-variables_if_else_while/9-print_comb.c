659e2e00cef48596cad7ca10b877517ce01c1abe
Abdullahi Yakubu Abu'Bakr <saffaree.sultante@gmail.com>
2023-03-19 18:12:50 -0700

    save this for now, lights are out will review and correct soon as lights are back

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -5,20 +5,20 @@
  * Description: 'print all possible combinations of single-digit numbers'
  * Return: always 0
  */
+
 int main(void)
 {
 	int i;
 
 	for (i = 0 ; i < 10 ; i++)
 	{
-		if (i == 9)
-			putchar(i + '0');
-		else
-		{
-			putchar(i + '0');
-			putchar(',');
-			putchar(' ');
-		}
+	       putchar(i = '0');
+	       if (i != 9)
+	       {
+	              putchar(',')
+	       	      putchar(' ')
+	       }		      
 	}
+	putchar('\n');
 	return (0);
 }
