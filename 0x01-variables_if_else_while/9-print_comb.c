8608f05c5108d7ea405f10083b2e183445a6e770
Abdullahi Yakubu Abu'Bakr <saffaree.sultante@gmail.com>
2023-03-20 14:46:15 -0700

    an unbeatable combination for success

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -2,7 +2,7 @@
 
 /**
  * main - main entry
- * Description: 'print all possible combinations of single-digit numbers'
+ * Description: 'print all combinations of single-digit numbers'
  * Return: always 0
  */
 
@@ -12,12 +12,12 @@ int main(void)
 
 	for (i = 0 ; i < 10 ; i++)
 	{
-	       putchar(i = '0');
-	       if (i != 9)
-	       {
-	              putchar(',')
-	       	      putchar(' ')
-	       }		      
+		putchar(i = '0');
+		if (i != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
 	putchar('\n');
 	return (0);
