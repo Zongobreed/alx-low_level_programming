4499fcd0330eff672ade08bd44b348bd0e733ea8
Abdullahi Yakubu Abu'Bakr <saffaree.sultante@gmail.com>
2023-03-29 10:29:39 -0700

    Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - function that changes the value of a given parameter
+ * @n: the parameter to changed
+ *
+ * Return: 0 (success)
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
