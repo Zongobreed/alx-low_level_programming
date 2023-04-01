e28436985cdc6330ac63c3caceae4dbaf310a24e
Abdullahi Yakubu Abu'Bakr <saffaree.sultante@gmail.com>
2023-04-01 16:47:51 -0700

    stuck but in recovery

--- 0x06-pointers_arrays_strings/7-main.c
+++ 0x06-pointers_arrays_strings/7-main.c
@@ -1,18 +1,17 @@
 #include "main.h"
 
 /**
- * main - check the code
+ * main - check the code for
  *
  * Return: Always 0.
  */
-
 int main(void)
 {
-	char s[] = "Expect the best. Prepare for the worst. Capitalize on what comes.\n";
-	char *p;
+    char s[] = "Expect the best. Prepare for the worst. Capitalize on what comes.\n";
+    char *p;
 
-	p = leet(s);
-	printf("%s", p);
-	printf("%s", s);
-	return (0);
+    p = leet(s);
+    printf("%s", p);
+    printf("%s", s);
+    return (0);
 }
