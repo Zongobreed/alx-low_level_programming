7829871200ca85172952458a73c564cfc0a17ab7
Abdullahi Yakubu Abu'Bakr <saffaree.sultante@gmail.com>
2023-03-31 20:11:13 -0700

    comparing two strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares strings
+ * @s1: parameter 1
+ * @s2: parameter 2
+ *
+ * Return: 0
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	i = 0;
+	while (s1[i] != '\0' && s2[i] != '\0')
+	{
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+		i++;
+	}
+	return (0);
+}
