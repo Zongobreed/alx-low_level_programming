ea18c1efd226a843c6138e58571648e859520a44
Abdullahi Yakubu Abu'Bakr <saffaree.sultante@gmail.com>
2023-04-06 20:08:31 -0700

    possibilities and probabilities

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+int actual_prime_number(int n, int i);
+
+/**
+ * is_prime_number - confirms if an integer is a prime number or not
+ * @n: input number
+ *
+ * Return: 1 (success) if not 0
+ */
+
+int is_prime_number(int n)
+{
+	if (n <= 1)
+		return (0);
+	return (actual_prime_number(n, n - 1));
+}
+
+/**
+ * actual_prime_number - recursively confirms if a number is prime
+ * @n: input number
+ * @i: iterator
+ *
+ * Return: 1 (success) if not 0
+ */
+
+int actual_prime_number(int n, int i)
+{
+	if (i == 1)
+		return (1);
+	if (n % i == 0 && i > 0)
+		return (0);
+	return (actual_prime_number(n, i - 1));
+}
