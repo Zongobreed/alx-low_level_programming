2eff498e8c6d8280c2f3c6443be13a2befd2e0c6
Abdullahi Yakubu Abu'Bakr <saffaree.sultante@gmail.com>
2023-04-14 20:57:16 -0700

    calloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+ * _memset - fills a memory area
+ * @s: mem area
+ * @b: char
+ * @n: bytes count
+ *
+ * Return: s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
+
+/**
+ * _calloc -  allocates memory for an array, using malloc
+ * @nmemb: number of elements in given array
+ * @size: size of element
+ *
+ * Return: ptr
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *ptr;
+
+	if (nmemb == 0)
+		return (NULL);
+	else if (size == 0)
+		return (NULL);
+	ptr = malloc(size * nmemb);
+	if (ptr == NULL)
+		return (NULL);
+	_memset(ptr, 0, nmemb * size);
+	return (ptr);
+}
