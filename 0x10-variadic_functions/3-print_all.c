3c6dd0d2b6a9fa03264fc56bead053adf5cc3fb9
Abdullahi Yakubu Abu'Bakr <saffaree.sultante@gmail.com>
2023-04-21 00:09:01 -0700

    value of a variable

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -10,4 +10,37 @@ void print_all(const char * const format, ...)
 	char *str, *gap = "";
 
 	va_list roll;
+
 	va_start(roll, format);
+	if (format)
+	{
+		while (format[i])
+		{
+			switch (format[i])
+			{
+				case 'c':
+					printf("%s%c", gap, va_arg(roll, int));
+					break;
+				case 'i':
+					printf("%s%d", gap, va_arg(roll, int));
+					break;
+				case 'f':
+					printf("%s%f", gap, va_arg(roll, double));
+					break;
+				case 's':
+					str = va_arg(roll, char *);
+					if (!str)
+						str = "(nil)";
+					printf("%s%s", gap, str);
+					break;
+				default:
+					i++;
+					continue;
+			}
+			gap = ", ";
+			i++;
+		}
+	}
+	printf("\n");
+	va_end(roll);
+}
