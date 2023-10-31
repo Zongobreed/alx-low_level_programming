5bb7cc85ec64872ad1030c3f290312f6ab0b7301
Abdullahi Yakubu Abu'Bakr <saffaree.sultanate@gmail.com>
2023-10-30 20:21:59 -0700

    Speak gently, she can hear

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -16,6 +16,9 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (!filename)
 		return (-1);
+	x = open(filename, O_WRONLY | O_APPEND);
+	if (x == -1)
+		return (-1);
 	if (text_content)
 	{
 		for (xlr = 0 ; text_content[xlr] ; xlr++)
