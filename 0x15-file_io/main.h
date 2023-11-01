36f585c56c5c2de44aa1181cf8bf105832db57ad
Abdullahi Yakubu Abu'Bakr <saffaree.sultanate@gmail.com>
2023-10-31 21:04:24 -0700

    Elf

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -8,7 +8,18 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <elf.h>
 
+void _check_elf(unsigned char *e_ident);
+void p_magic(unsigned char *e_ident);
+void p_class(unsigned char *e_ident);
+void p_data(unsigned char *e_ident);
+void p_version(unsigned char *e_ident);
+void p_abi(unsigned char *e_ident);
+void p_osabi(unsigned char *e_ident);
+void p_type(unsigned int e_type, unsigned char *e_ident);
+void p_entry(unsigned long int e_entry, unsigned char *e_ident);
+void _close_elf(int elf);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
